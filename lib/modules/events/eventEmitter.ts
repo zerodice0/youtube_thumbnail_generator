059318e724f79f5166e7cb5d44654dd346721e7f
zerodice0 <zerodice0@gmail.com>
2025-03-28 00:06:57 +0900

    Add EventEmitter class for managing JobQueue event connection.

---
+++ lib/modules/events/eventEmitter.ts
@@ -0,0 +1,27 @@
+class EventEmitter {
+  private static connections = new Map<string, WritableStreamDefaultWriter>();
+
+  public static addConnection(id: string, writer: WritableStreamDefaultWriter) {
+    this.connections.set(id, writer);
+  }
+
+  public static removeConnection(id: string) {
+    this.connections.delete(id);
+  }
+
+  public static async emit(event: string, data: any) {
+    const message = `data: ${JSON.stringify({ event, data })}\n\n`;
+    const encoder = new TextEncoder();
+
+    Array.from(this.connections.entries()).forEach(async ([id, writer]) => {
+      try {
+        await writer.write(encoder.encode(message));
+      } catch (error) {
+        console.error(`Failed to send message to connection ${id}:`, error);
+        this.removeConnection(id);
+      }
+    });
+  }
+}
+
+export default EventEmitter;
