0e422ba1e89b01ba9124ef73e11d7800fa8e7a95
zerodice0 <zerodice0@gmail.com>
2025-04-08 08:58:43 +0900

    status 상태값에 따라 네 개의 스토리 추가.
    - 컴포넌트를 스토리북으로 관리하게 되면서, 더 이상 더미 데이터를 사용할 일은 없으므로 dev:dummy 스크립트와 환경변수를 제거함.
    - dev:dummy 스크립트와 환경변수를 제거했으므로, 환경변수값 USE_DUMMY_DATA을 참조하는 코드를 제거함.

--- components/jobStateCard/jobStateCard.stories.tsx
+++ components/jobStateCard/jobStateCard.stories.tsx
@@ -8,20 +8,77 @@ const meta = {
 
 export default meta;
 
-type Story = StoryObj<typeof meta>;
+type DefaultStory = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const Default: DefaultStory = {
   args: {
     job: {
 			id: '1',
 			createdAt: new Date(),
 			status: 'queued',
-			thumbnailUrl: 'https://via.placeholder.com/150',
-			audioFilePath: 'https://via.placeholder.com/150',
+			thumbnailUrl: 'https://placehold.co/600x400',
+			audioFilePath: 'https://placehold.co/600x400',
 			summary: 'This is a summary',
 			youtubeUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
 			yotubeTitle: 'This is a title',
-			transcriptFilePath: 'https://via.placeholder.com/150',
+			transcriptFilePath: 'https://placehold.co/600x400',
+			priority: 1,
+		},
+  }
+};
+
+type ProcessingStory = StoryObj<typeof meta>;
+
+export const Processing: ProcessingStory = {
+  args: {
+    job: {
+			id: '1',
+			createdAt: new Date(),
+			status: 'processing',
+			thumbnailUrl: 'https://placehold.co/600x400',
+			audioFilePath: 'https://placehold.co/600x400',
+			summary: 'This is a summary',
+			youtubeUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+			yotubeTitle: 'This is a title',
+			transcriptFilePath: 'https://placehold.co/600x400',
+			priority: 1,
+		},
+  }
+};
+
+type CompletedStory = StoryObj<typeof meta>;
+
+export const Completed: CompletedStory = {
+  args: {
+    job: {
+			id: '1',
+			createdAt: new Date(),
+			status: 'completed',
+			thumbnailUrl: 'https://placehold.co/600x400',
+			audioFilePath: 'https://placehold.co/600x400',
+			summary: 'This is a summary',
+			youtubeUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+			yotubeTitle: 'This is a title',
+			transcriptFilePath: 'https://placehold.co/600x400',
+			priority: 1,
+		},
+  }
+};
+
+type FailedStory = StoryObj<typeof meta>;
+
+export const Failed: FailedStory = {
+  args: {
+    job: {
+			id: '1',
+			createdAt: new Date(),
+			status: 'failed',
+			thumbnailUrl: 'https://placehold.co/600x400',
+			audioFilePath: 'https://placehold.co/600x400',
+			summary: 'This is a summary',
+			youtubeUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+			yotubeTitle: 'This is a title',
+			transcriptFilePath: 'https://placehold.co/600x400',
 			priority: 1,
 		},
   }
--- lib/modules/events/jobQueue.ts
+++ lib/modules/events/jobQueue.ts
@@ -20,46 +20,6 @@ class JobQueue {
 
   private constructor() {
     this.queue = new PriorityQueue<Job>((a, b) => a - b);
-
-    if (process.env.USE_DUMMY_DATA === 'true') {
-      console.log('USE_DUMMY_DATA is true');
-      this.addJob({
-        id: '1',
-        youtubeUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-        yotubeTitle: '[test] Youtube 타이틀',
-        thumbnailUrl: 'https://placehold.co/600x400',
-        audioFilePath: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-        transcriptFilePath: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-        priority: 1,
-        status: 'queued',
-        createdAt: new Date(Date.now()),
-        summary: 'test',
-      });
-      this.addJob({
-        id: '2',
-        youtubeUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-        yotubeTitle: '[test] Youtube 타이틀2',
-        thumbnailUrl: 'https://placehold.co/600x400',
-        audioFilePath: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-        transcriptFilePath: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-        priority: 1,
-        status: 'queued',
-        createdAt: new Date(Date.now()),
-        summary: 'test2',
-      });
-      this.addJob({
-        id: '3',
-        youtubeUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-        yotubeTitle: '[test] Youtube 타이틀3',
-        thumbnailUrl: 'https://placehold.co/600x400',
-        audioFilePath: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-        transcriptFilePath: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-        priority: 1,
-        status: 'queued',
-        createdAt: new Date(Date.now()),
-        summary: 'test3',
-      });
-    }
   }
 
   public static getInstance(): JobQueue {
@@ -71,9 +31,7 @@ class JobQueue {
 
   public addJob(job: Job): void {
     this.queue.enqueue(job, job.priority);
-    if (process.env.USE_DUMMY_DATA !== 'true') {
-      this.processNextJob(); // 새 작업이 추가되면 처리 시도
-    }
+    this.processNextJob(); // 새 작업이 추가되면 처리 시도
     EventEmitter.emit('jobAdded', job);
   }
 
